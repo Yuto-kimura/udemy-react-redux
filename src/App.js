513317ad1154a6aa9e2d979ecfcf2dcbbcf109ae
Yuto-kimura <s1250172a@gmail.com>
2020-10-01 17:24:02 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
 import React from "react";
 
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input
-//         type="text"
-//         onChange={() => {
-//           console.log("I am clicked");
-//         }}
-//       ></input>
-//     </React.Fragment>
-//   );
-// }
-
-const App = () => {
+function App() {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "noname" },
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profiles, index) => {
+        return <User name={profiles.name} age={profiles.age} key={index} />;
+      })}
+    </div>
+  );
+}
+const User = (props) => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old!
     </div>
   );
 };
-const Cat = () => {
-  return <div>Meow!</div>;
+
+User.defaultProps = {
+  age: 1,
 };
 export default App;
