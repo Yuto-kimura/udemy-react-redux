df418fad5970bf9e6be0a7d86a9687c11ada23d6
Yuto-kimura <s1250172a@gmail.com>
2020-10-02 17:19:49 +0900

    implement component with conncet function

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,17 +1,16 @@
-import {INCREMENT,DECREMENT} from ../actions
+import { INCREMENT, DECREMENT } from "../actions";
 
-const initialState = {value: 0 }
+const initialState = { value: 0 };
 
-default(state=initialState,action)=>{
-    switch (action.type) {
-        case 'INCREMENT':
-            return {value: state.value + 1}
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
 
-        case 'DECREMENT':
-            return {value: state.value - 1}
-        
-    
-        default:
-            return state
-    }
-}
+    case "DECREMENT":
+      return { value: state.value - 1 };
+
+    default:
+      return state;
+  }
+};
